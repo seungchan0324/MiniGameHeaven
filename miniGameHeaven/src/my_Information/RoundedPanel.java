787ba1473262a116a29e3fabcf5426886cca1685
seungchan0324 <winchan324@gmail.com>
2024-03-25 17:21:21 +0900

    my_Information 변경

--- miniGameHeaven/src/my_Information/RoundedPanel.java
+++ miniGameHeaven/src/my_Information/RoundedPanel.java
@@ -1,27 +1,27 @@
-package interfaces;
+package my_Information;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-class RoundedLabel extends JLabel {
-    private int radius;
+class RoundedPanel extends JPanel {
+	private int radius;
 
-    public RoundedLabel(int radius) {
-        this.radius = radius;
-        setOpaque(false);
-    }
+	public RoundedPanel(int radius) {
+		this.radius = radius;
+		setOpaque(false); // 투명 배경을 위해 설정
+	}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-    	super.paintComponent(g);
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		int width = getWidth();
 		int height = getHeight();
 		Graphics2D graphics = (Graphics2D) g;
 		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		graphics.setColor(getBackground());
 		graphics.fillRoundRect(0, 0, width, height, radius, radius);
-    }
+	}
+
 }
