af00c4cb5f6f1c33a170bacc15345b3813131cb7
shuping132 <gusals8579@naver.com>
2024-04-02 12:40:43 +0900

    게임설명란 내 정보로 가기 버튼 추가

--- miniGameHeaven/src/gameDescription/GameDescription.java
+++ miniGameHeaven/src/gameDescription/GameDescription.java
@@ -1,6 +1,7 @@
 package gameDescription;
 
 import java.awt.Color;
+
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -8,7 +9,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -23,6 +23,7 @@ import YeonGame.Umi;
 import defaultFrame.DefaultFrame;
 import infinityStair.StartFrame;
 import infinityStair.StartPanel;
+import my_Information.MyInformationPanel;
 
 public class GameDescription extends JPanel {
 	private JPanel mainPanel;
@@ -35,17 +36,12 @@ public class GameDescription extends JPanel {
 
 	// 게임 정보를 담을 배열
 	private String[] gameNames = { "날아! 날아!", "우주로", "바다이야기", "이슬비", "penalty24", "클릭클릭" };
-	private String[] gameDescriptions = { "날아!날아!는 숲속 친구들을 스페이스바 만으로 배관에 부딛치지 않고 피해 끝까지 날아가 최고점수를 따내는 게임이다.", "게임2 설명", "바다에 가서 잠수를 해 보자!\n"
-			+ "어쩌면 바닷속에 숨겨진 보물을 찾을 수 있을지도…?!",
-			"영어 타자가 느리다고 ?!\n"
-			+ "걱정마 ! 내가 도와줄게 ! 시간은 30초 !\n"
-			+ "카테고리 별로 떨어지는 단어들을 최대한 빠르고 정확하게 입력해 봐 !\n"
-			+ "즐겁게 영어 타자 연습도 하고 포인트도 얻어가자 !"
-			, "공을 다섯번찹니다 왼쪽, 가운데, 오른쪽 셋중 원하는 위치로 공을 차서 골을 성공시켜야 합니다 다섯번중 3번 이상 골을 넣으면 게임 에서 승리하여 n포인트를  받습니다.",
-					"아무생각없이 하기 좋은 게임\n"
-					+ "알을 마구마구 클릭하세요 3번 클릭당 1점\n"
-					+ "레벨이 바뀔때마다 배경도 바뀜.\n"
-					+ "노가다의 끝판왕 최고점에 도전하세요" };
+	private String[] gameDescriptions = { "날아!날아!는 숲속 친구들을 스페이스바 만으로 배관에 부딛치지 않고 피해 끝까지 날아가 최고점수를 따내는 게임이다.", "게임2 설명",
+			"바다에 가서 잠수를 해 보자!\n" + "어쩌면 바닷속에 숨겨진 보물을 찾을 수 있을지도…?!",
+			"영어 타자가 느리다고 ?!\n" + "걱정마 ! 내가 도와줄게 ! 시간은 30초 !\n" + "카테고리 별로 떨어지는 단어들을 최대한 빠르고 정확하게 입력해 봐 !\n"
+					+ "즐겁게 영어 타자 연습도 하고 포인트도 얻어가자 !",
+			"공을 다섯번찹니다 왼쪽, 가운데, 오른쪽 셋중 원하는 위치로 공을 차서 골을 성공시켜야 합니다 다섯번중 3번 이상 골을 넣으면 게임 에서 승리하여 n포인트를  받습니다.",
+			"아무생각없이 하기 좋은 게임\n" + "알을 마구마구 클릭하세요 3번 클릭당 1점\n" + "레벨이 바뀔때마다 배경도 바뀜.\n" + "노가다의 끝판왕 최고점에 도전하세요" };
 	// 게임 이미지 경로나 아이콘을 사용할 수 있습니다. \
 	private String[] gameImages = { "flyingflying.gif", "into space.gif", "the story of the sea.gif", "sprinkle.gif",
 			"penalty24.gif", "ClickClick.gif" };
@@ -79,7 +75,7 @@ public class GameDescription extends JPanel {
 		mainPanel.setBounds(0, 0, 768, 600);
 		mainPanel.setBackground(new Color(255, 255, 244));
 		add(mainPanel);
-		this.currentGameIndex=currentGameIndex;
+		this.currentGameIndex = currentGameIndex;
 		setupUI();
 		updateGameInfo(currentGameIndex);
 
@@ -88,21 +84,43 @@ public class GameDescription extends JPanel {
 
 	private void setupUI() {
 		// 상단에 메인 화면으로 가는 버튼
-		RoundedButton button = new RoundedButton(40);
-		JLabel EndLabel = new JLabel("메인 화면");
-		EndLabel.setFont(new Font("맑은 고딕", Font.BOLD, 15));
-		EndLabel.setBounds(78, 50, 100, 40);
-		EndLabel.setForeground(Color.white);
-		button.setBackground(new Color(94, 94, 94));
-		button.setBounds(60, 50, 100, 40);
-		mainPanel.add(EndLabel);
-		mainPanel.add(button);
+		RoundedButton mainbutton = new RoundedButton(40);
+		JLabel mainLabel = new JLabel("메인 화면");
+		mainLabel.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		mainLabel.setBounds(78, 50, 100, 40);
+		mainLabel.setForeground(Color.white);
+		mainbutton.setBackground(new Color(94, 94, 94));
+		mainbutton.setBounds(60, 50, 100, 40);
+		
+		// 상단에 내정로 화면ㅇ로 가는 버튼
+		RoundedButton infomebutton = new RoundedButton(40);
+		JLabel infomeLabel = new JLabel("내 정보");
+		infomeLabel.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		infomeLabel.setBounds(202, 50, 100, 40);
+		infomeLabel.setForeground(Color.white);
+		infomebutton.setBackground(new Color(94, 94, 94));
+		infomebutton.setBounds(180, 50, 100, 40);
+		
+		//버튼생성
+		mainPanel.add(mainLabel);
+		mainPanel.add(mainbutton);
+		mainPanel.add(infomeLabel);
+		mainPanel.add(infomebutton);
 
-		button.addActionListener(new ActionListener() {
+		// 메인화면버튼 이벤트
+		mainbutton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				SwingUtilities.getWindowAncestor(button).setVisible(false);
+				SwingUtilities.getWindowAncestor(mainbutton).setVisible(false);
 				DefaultFrame.getInstance(new Main_Interface(), "메인 화면");
+			}
+		});
+		// 내정보버튼 이벤트
+		infomebutton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				SwingUtilities.getWindowAncestor(infomebutton).setVisible(false);
+				DefaultFrame.getInstance(new MyInformationPanel(), "내 정보");
 			}
 		});
 
@@ -253,7 +271,7 @@ public class GameDescription extends JPanel {
 			}
 			// 변경된 색상으로 UI를 업데이트
 
-		} 
+		}
 	}
 
 	private void updateGameInfo(int gameIndex) {
@@ -275,13 +293,5 @@ public class GameDescription extends JPanel {
 
 	}
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new GameDescription();
-			}
-		});
-	}
 
 }
