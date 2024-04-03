7397c4a5657c7875c38e75d26c935e69a407f5e4
soldesk <soldesk@DESKTOP-3B475RD>
2024-04-03 18:10:58 +0900

    sdw

--- miniGameHeaven/src/login/Logintest.java
+++ miniGameHeaven/src/login/Logintest.java
@@ -205,21 +205,25 @@ public class Logintest extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (userIdField.getText().equals("soldesk") || Register.hm.containsKey(userIdField.getText())) {
-					if (new String(passwordField.getPassword()).equals("1234")) {
-						name = "soldesk";
-						JOptionPane.showMessageDialog(null, "soldesk님 반갑습니다!");
-						SwingUtilities.getWindowAncestor(loginButton).setVisible(false);
-						DefaultFrame.getInstance(new Main_Interface(), "메인 화면");
-						MyInformationPanel.money = 100000;
-					} else if (Register.hm.get(userIdField.getText()).getPass()
-							.equals(new String(passwordField.getPassword()))) {
-						name = Register.hm.get(userIdField.getText()).getID();
-						JOptionPane.showMessageDialog(null,
-								Register.hm.get(userIdField.getText()).getEmail() + "님 반갑습니다!");
-						SwingUtilities.getWindowAncestor(loginButton).setVisible(false);
-						DefaultFrame.getInstance(new Main_Interface(), "메인 화면");
-						MyInformationPanel.money = Register.hm.get(userIdField.getText()).getPoint();
-					} else {
+					try {
+						if (new String(passwordField.getPassword()).equals("1234")) {
+							name = "soldesk";
+							JOptionPane.showMessageDialog(null, "soldesk님 반갑습니다!");
+							SwingUtilities.getWindowAncestor(loginButton).setVisible(false);
+							DefaultFrame.getInstance(new Main_Interface(), "메인 화면");
+							MyInformationPanel.money = 100000;
+						} else if (Register.hm.get(userIdField.getText()).getPass()
+								.equals(new String(passwordField.getPassword()))) {
+							name = Register.hm.get(userIdField.getText()).getID();
+							JOptionPane.showMessageDialog(null,
+									Register.hm.get(userIdField.getText()).getEmail() + "님 반갑습니다!");
+							SwingUtilities.getWindowAncestor(loginButton).setVisible(false);
+							DefaultFrame.getInstance(new Main_Interface(), "메인 화면");
+							MyInformationPanel.money = Register.hm.get(userIdField.getText()).getPoint();
+						} else {
+							JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호를 틀리셨습니다.");
+						}
+					} catch (Exception e2) {
 						JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호를 틀리셨습니다.");
 					}
 				} else {
